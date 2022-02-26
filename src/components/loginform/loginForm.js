407c3c8021fd10e7dde03704eb59717e93df5fc8
EzikeChris <christophermbe1009@gmail.com>
2022-02-26 17:01:39 +0100

    add file

--- src/components/loginform/loginForm.js
+++ src/components/loginform/loginForm.js
@@ -1,3 +1,4 @@
 import React, { useState } from 'react';
 import OktaAuth from '@okta/okta-auth-js';
 import { useOktaAuth } from '@okta/okta-react'
+import { Form, Input, Button, Alert, Row, Col } from 'antd';
