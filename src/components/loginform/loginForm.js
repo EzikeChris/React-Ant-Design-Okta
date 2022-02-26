5e01c2f5b166d2da092f24c6e1de3e233cf48adb
EzikeChris <christophermbe1009@gmail.com>
2022-02-26 17:03:40 +0100

    1 file modified

--- src/components/loginform/loginForm.js
+++ src/components/loginform/loginForm.js
@@ -2,3 +2,10 @@ import React, { useState } from 'react';
 import OktaAuth from '@okta/okta-auth-js';
 import { useOktaAuth } from '@okta/okta-react'
 import { Form, Input, Button, Alert, Row, Col } from 'antd';
+
+
+
+const LoginForm = ({baseUrl, issuer}) => {
+  const { authService } = useOktaAuth();
+  
+}
