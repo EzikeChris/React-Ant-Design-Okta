d7eadda9ecb8884db6bd1cefb2fd7c1ea284f150
EzikeChris <christophermbe1009@gmail.com>
2022-02-26 17:11:35 +0100

    2 files modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./style.css";
+import LoginForm from './components/loginform/loginform'
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <LoginForm/>
     </div>
   );
 }
